759c54f74d8b7f281f18900b3f8bbcde68df3652
arya <aryadad15@gmail.com>
2026-01-09 17:22:18 -0500

    Initial Commit

---
+++ test2/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  test2
+//
+//  Created by Arya Rahimian on 2026-01-09.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ test2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Arya Rahimian on 2026-01-09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
